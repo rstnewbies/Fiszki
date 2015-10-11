d9e2ed7055bb50bb8ec0dbbb7fcab9d6f35f280e
Mateusz Marciniszyn <marciniszyn@pwapm.com>
2015-10-11 17:10:24 +0200

    small fix

--- app/src/main/java/eu/qm/fiszki/MainActivity.java
+++ app/src/main/java/eu/qm/fiszki/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timer.start(this,5000,getString(R.string.notification_message),getString(R.string.notification_title));
+        timer.start(this,900000,getString(R.string.notification_message),getString(R.string.notification_title));
         openDataBase.openDB(myDb);
         populateListView();
     }
