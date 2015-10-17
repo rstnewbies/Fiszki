8768e948573a92314c397cc2a149c656c669cb1b
Mateusz Marciniszyn <marciniszyn@pwapm.com>
2015-10-17 20:39:38 +0200

    Fixed notification with null database

--- app/src/main/java/eu/qm/fiszki/TimerClass.java
+++ app/src/main/java/eu/qm/fiszki/TimerClass.java
@@ -5,16 +5,19 @@ import android.os.CountDownTimer;
 
 public class TimerClass {
     NotificationsClass notification = new NotificationsClass();
+    public void start(final Context window, int time, final String notificationMessage, final String notificationTitle, final DBAdapter myDB) {
 
-    public void start(final Context window, int time, final String notificationMessage, final String notificationTitle) {
         CountDownTimer Count = new CountDownTimer(time, 1000) {
             public void onTick(long millisUntilFinished) {
             }
 
             public void onFinish() {
-                notification.CreateNotification(window,notificationMessage,notificationTitle);
+                if(myDB.rowCount()>0) {
+                    notification.CreateNotification(window, notificationMessage, notificationTitle);
+                }
                 start();
             }
+
 
         };
         Count.start();
