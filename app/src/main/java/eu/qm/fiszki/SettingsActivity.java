7f9daf526809d1bfbf864b0445c395ce2c6aa444
Michal Pawlicki <michalpawlicki@outlook.com>
2015-10-31 13:35:40 +0100

    #16 Settings Activity and layout, not working switch (for now), added back button, deleted "stop" button in main activity; fixed menu_main and

--- app/src/main/java/eu/qm/fiszki/SettingsActivity.java
+++ app/src/main/java/eu/qm/fiszki/SettingsActivity.java
@@ -1,12 +1,14 @@
 package eu.qm.fiszki;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
-
 import eu.qm.fiszki.ActivityContainer.MainActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -23,8 +25,9 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if(isChecked){
-                  MainActivity alarm = new MainActivity();
-                  alarm.notOff();
+                 // MainActivity alarm = new MainActivity();
+                  //alarm.notOff();
+                    Toast.makeText(getApplicationContext(), "Wylaczony switch", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Wylaczony switch", Toast.LENGTH_SHORT).show();
@@ -40,6 +43,24 @@ public class SettingsActivity extends AppCompatActivity {
         else{
             Toast.makeText(getApplicationContext(), "Wylaczony switch", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Intent goHome = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(goHome);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
