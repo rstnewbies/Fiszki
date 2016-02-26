57cabfe521561b69660086753ec610e456f87551
Mateusz 'mBoiler' Marciniszyn <marciniszyn@pwapm.com>
2016-02-26 17:39:32 +0100

    Merged refactor/develop to develop

---
+++ app/src/test/java/eu/qm/fiszki/RulesTest.java
@@ -0,0 +1,25 @@
+package eu.qm.fiszki;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mBoiler on 26.02.2016.
+ */
+public class RulesTest {
+
+    Rules tRules;
+
+    @Before
+    public void setUp() throws Exception {
+        tRules = new Rules();
+    }
+
+    @Test
+    public void testCheck() throws Exception {
+        assertTrue(tRules.Check(new String("chuj"),new String("chuj")));
+        assertFalse(tRules.Check(new String("chuj"),new String("jaro")));
+    }
+}
